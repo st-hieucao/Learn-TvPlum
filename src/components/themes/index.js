bf8a89678e04c77f7e0a8a5de09d9cd9a5781e77
CaoKhaHieu <caokhahieu9@gmail.com>
2022-11-03 09:34:51 +0700

    fix

--- src/components/themes/index.js
+++ src/components/themes/index.js
@@ -6,13 +6,13 @@ export const ThemeContext = React.createContext('light');
 export const ThemeProvider = (props) => {
   const [theme, setTheme] = useState('dark');
 
-  useEffect(() => {
-    const defaultTheme = themes.default[theme].colors;
-    Object.keys(defaultTheme).map((key) => {
-      const cssKey = `--${key}`;
-      document.body.style.setProperty(cssKey, defaultTheme[key]);
-    })
-  }, [theme]);
+  // useEffect(() => {
+  //   const defaultTheme = themes.default[theme].colors;
+  //   Object.keys(defaultTheme).map((key) => {
+  //     const cssKey = `--${key}`;
+  //     document.body.style.setProperty(cssKey, defaultTheme[key]);
+  //   })
+  // }, [theme]);
 
 
   const handleChangeTheme = (theme) => {
